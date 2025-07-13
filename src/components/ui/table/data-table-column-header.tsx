965e95b8de7bfd0fd8d1b85de7fdde56a9665aa5
herlmanoel <herlmanoelfernandes@gmail.com>
2025-07-13 17:14:09 -0300

    feat: traduzir textos de ordenação e visibilidade na tabela de cabeçalho

--- src/components/ui/table/data-table-column-header.tsx
+++ src/components/ui/table/data-table-column-header.tsx
@@ -62,7 +62,7 @@ export function DataTableColumnHeader<TData, TValue>({
               onClick={() => column.toggleSorting(false)}
             >
               <ChevronUpIcon />
-              Asc
+              Crescente
             </DropdownMenuCheckboxItem>
             <DropdownMenuCheckboxItem
               className='[&_svg]:text-muted-foreground relative pr-8 pl-2 [&>span:first-child]:right-2 [&>span:first-child]:left-auto'
@@ -70,7 +70,7 @@ export function DataTableColumnHeader<TData, TValue>({
               onClick={() => column.toggleSorting(true)}
             >
               <ChevronDownIcon />
-              Desc
+              Decrescente
             </DropdownMenuCheckboxItem>
             {column.getIsSorted() && (
               <DropdownMenuItem
@@ -78,7 +78,7 @@ export function DataTableColumnHeader<TData, TValue>({
                 onClick={() => column.clearSorting()}
               >
                 <Cross2Icon />
-                Reset
+                Limpar ordenação
               </DropdownMenuItem>
             )}
           </>
@@ -90,7 +90,7 @@ export function DataTableColumnHeader<TData, TValue>({
             onClick={() => column.toggleVisibility(false)}
           >
             <EyeOff />
-            Hide
+            Esconder coluna
           </DropdownMenuCheckboxItem>
         )}
       </DropdownMenuContent>
